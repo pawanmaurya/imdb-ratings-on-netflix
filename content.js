dec046a00a325df4f0a3c9a0195a21e311fccef9
Miguel Ávila Monroy <soyer893@gmail.com>
2019-02-21 16:27:12 +0100

    Add rotten tomatoes rating flag to the request

--- content.js
+++ content.js
@@ -1,22 +1,20 @@
-function fetchMovieNameYear(){
+function fetchMovieNameYear() {
 	var synopsis = document.querySelectorAll('.jawBone .jawbone-title-link');
-  	if(synopsis === null){
-  		return;
-  	}
+  if(synopsis === null){
+ 		return;
+	}
 
-  	var logoElement = document.querySelectorAll('.jawBone .jawbone-title-link .title');
+	var logoElement = document.querySelectorAll('.jawBone .jawbone-title-link .title');
 
-  	if(logoElement.length === 0)
-  		return;
+	if(logoElement.length === 0)
+		return;
 
-  	logoElement = logoElement[logoElement.length - 1];
+	logoElement = logoElement[logoElement.length - 1];
 
-  	var title = logoElement.textContent;
+	var title = logoElement.textContent;
 
-
-  	if(title === ""){
-  		title = logoElement.querySelector(".logo").getAttribute("alt");
-  	}
+  if(title === "")
+  	title = logoElement.querySelector(".logo").getAttribute("alt");
 
 	var	titleElement = document.querySelectorAll('.jawBone .jawbone-title-link .title .text').textContent;
 
@@ -25,21 +23,22 @@ function fetchMovieNameYear(){
 		return;
 	var year = yearElement[yearElement.length-1].textContent;
 
-
 	var divId = getDivId(title, year);
-
 
 	if(document.querySelector("#"+divId) !== null && $("#"+divId).is(":visible")){
 		return;
 	}
 
-	var existingImdbRating = sessionStorage.getItem(title+":"+year);
-	if((existingImdbRating !== "undefined") && (existingImdbRating !== null))
+	var existingImdbRating = window.sessionStorage.getItem(title+":"+year);
+	if((existingImdbRating !== "undefined") && (existingImdbRating !== null)) {
 		addIMDBRating(existingImdbRating, title, year);
-	else
-		var imdbRating = getImdbRating(title,year);
-	
-}
+		//if imdbRating is fetched, rotten is fetched too
+		var rottenRating = window.sessionStorage.getItem("rotten"+title+":"+year);
+		addRottenRating(rottenRating, title, year);
+	} else {
+		getRatings(title,year);
+	}
+};
 
 function addIMDBRating(imdbRating, name, year){
 	var divId = getDivId(name, year);
@@ -48,11 +47,19 @@ function addIMDBRating(imdbRating, name, year){
 		return;
 	}
 
-	var elems = document.querySelectorAll(".jawBone .synopsis").length;
-
-  	
 	$('.jawBone .synopsis').last().before(
 		'<div class="imdbRating" id="'+divId+'">'+'IMDb rating : ' + ((imdbRating && imdbRating != 'undefined')?imdbRating:"N/A"));
+}
+
+function addRottenRating(rottenRating, name, year){
+	var divId = `rotten${getDivId(name, year)}`;
+
+	if(document.querySelector("#"+divId) !== null && $("#"+divId).is(":visible")){
+		return;
+	}
+
+	$('.jawBone .synopsis').last().before(
+		'<div class="rottenRating" id="'+divId+'">'+'Rotten Tomatoes rating : ' + ((rottenRating && rottenRating != 'undefined')?rottenRating:"N/A"));
 }
 
 function getDivId(name, year){
@@ -63,51 +70,59 @@ function getDivId(name, year){
 
 
 $(document).ready(function(event) {
+	if(window.sessionStorage === "undefined")
+		return;
 
+	var target = document.body;
+	// create an observer instance
+	var observer = new MutationObserver(function(mutations) {
+	  mutations.forEach(function(mutation) {
+	  	window.setTimeout(fetchMovieNameYear,5);
+	  });
+	});
+	// configuration of the observer:
+	var config = {
+	  attributes: true,
+	  childList: true,
+	  characterData: true
+	};
 
-if(window.sessionStorage === "undefined")
-	return;
-
-var target = document.body;
-// create an observer instance
-var observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-  	window.setTimeout(fetchMovieNameYear,5); 
-  });
-});
-// configuration of the observer:
-var config = {
-  attributes: true,
-  childList: true,
-  characterData: true
-};
-
-observer.observe(target, config);
-
+	observer.observe(target, config);
 });
 
-function getImdbRating(name, year){
+function getRatings(name, year){
 	var existingImdbRating = window.sessionStorage.getItem(name+":"+year);
 	if((existingImdbRating !== "undefined") && (existingImdbRating !== null)){
 		addIMDBRating(existingImdbRating, name, year);
-		return;
+		//if imdbRating is fetched, rotten is fetched too
+		var rottenRating = window.sessionStorage.getItem("rotten"+name+":"+year);
+		addRottenRating(rottenRating, name, year);
+	} else {
+		var url = "https://www.omdbapi.com/?apikey=<secret_key>&t="+encodeURI(name)+"&y="+year+"tomatoes=true";
+		$.ajax({
+									type: "GET",
+									url: url,
+									contentType: 'application/json',
+									xhrFields: {
+								withCredentials: true
+						},
+									dataType: 'json',
+									async : false,
+									success: function (apiResponse) {
+										var imdbRating = apiResponse["imdbRating"];
+										var rottenRating = extractRottenTomatoesRating(apiResponse["Ratings"]);
+										window.sessionStorage.setItem(name+":"+year, imdbRating);
+										window.sessionStorage.setItem("rotten:"+name+":"+year, rottenRating);
+										addIMDBRating(imdbRating, name, year);
+										addRottenRating(rottenRating, name, year);
+									},
+									error: function (data) {
+									}
+			});
 	}
-	var url = "https://www.omdbapi.com/?apikey=<secret_key>&t="+encodeURI(name)+"&y="+year;
-	$.ajax({
-                type: "GET",
-                url: url,
-                contentType: 'application/json',
-                xhrFields: {
-    			    withCredentials: true
-    			},
-                dataType: 'json',
-                async : false,
-                success: function (apiResponse) {
-                	var imdbRating = apiResponse["imdbRating"];
-                	window.sessionStorage.setItem(name+":"+year,imdbRating);
-                	addIMDBRating(imdbRating, name, year);
-                },
-                error: function (data) {
-                }
-    });
+};
+
+function extractRottenTomatoesRating(ratings) {
+	const rottenRating = ratings.filter( rating => rating.Source === "Rotten Tomatoes")
+	return rottenRating[0] ? rottenRating[0].Value : undefined
 };
